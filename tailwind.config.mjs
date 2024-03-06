712741f4280c0e0450df8717328440a2259bee23
Paloma Wink <paloma.wink@hotmail.com>
2024-03-05 22:40:32 -0300

    raw: Adiconando estilos extendidos ao tailwind e ajustando configura

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      fontFamily: {
+        'raleway': ['Raleway', 'sans-serif'],
+        'roboto': ['Roboto', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif'],
+        'playfair': ['Playfair Display', 'serif'],
+      },
+      colors: {
+        'secundary': '#89CFF0',
+        'tertiary': '#F4A1A1',
+        'quaternary': '#98FF98',
+        'quinary': '#E6E6FA',
+        'verde-claro': '#80D8DA',
+        'rosa-pessego': '#F8B7B7',
+      },
+    },
 	},
 	plugins: [],
 }

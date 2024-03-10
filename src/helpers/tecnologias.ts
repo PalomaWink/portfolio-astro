772b6e80cadee127edab6148c6abbb07dcdfbc26
Paloma Wink <paloma.wink@hotmail.com>
2024-03-10 01:04:17 -0300

    feat: Criando componente para as tecnologias

---
+++ src/helpers/tecnologias.ts
@@ -0,0 +1,36 @@
+import css from '../../public/img/tecnologias/css-3.svg'
+import eslint from '../../public/img/tecnologias/eslint.svg'
+import git from '../../public/img/tecnologias/git-icon.svg'
+import github from '../../public/img/tecnologias/github-octocat.svg'
+import javascript from '../../public/img/tecnologias/javascript.svg'
+import jest from '../../public/img/tecnologias/jest.svg'
+import next from '../../public/img/tecnologias/next-js.svg'
+import nodeJS from '../../public/img/tecnologias/node-js.svg'
+import react from '../../public/img/tecnologias/react.svg'
+import redux from '../../public/img/tecnologias/redux.svg'
+import tailwind from '../../public/img/tecnologias/tailwindcss.svg'
+import sass from '../../public/img/tecnologias/sass.svg'
+import typescript from '../../public/img/tecnologias/typescript.svg'
+import vercel from '../../public/img/tecnologias/vercel.svg'
+import testingLibrary from '../../public/img/tecnologias/testing-library.svg'
+
+export const firstBlock = [
+  {src: react, alt: "React"},
+  {src: next, alt: "Next.JS"},
+  {src: typescript, alt: "TypeScript"},
+  {src: javascript, alt: "JavaScript"},
+  {src: redux, alt: "Redux"},
+  {src: tailwind, alt: "Tailwind CSS"},
+  {src: sass, alt: "SASS"},
+  {src: css, alt: "CSS3"},
+]
+
+export const secondBlock = [
+  {src: testingLibrary, alt: "Testing Library"},
+  {src: jest, alt: "Jest"},
+  {src: eslint, alt: "ESLint"},
+  {src: git, alt: "Git"},
+  {src: github, alt: "GitHub"},
+  {src: vercel, alt: "Vercel"},
+  {src: nodeJS, alt: "Node.JS"},
+]
